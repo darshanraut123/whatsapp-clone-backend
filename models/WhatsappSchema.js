0ec948d5555bb382eee07dd44acb9f44ebe4775b
darshanraut123 <darshanraut3828@gmail.com>
2022-03-29 01:07:43 +0530

    Done with all changes

--- models/WhatsappSchema.js
+++ models/WhatsappSchema.js
@@ -1,11 +1,11 @@
 const mongoose = require("mongoose");
 
 const whatsappSchema = mongoose.Schema({
-    message: {type: String,required: true},
-    name: {type: String,required: true},
-    timestamp: {type: String,required: false},
-    received: {type:Boolean}
-    
+    message: { type: String, required: true },
+    name: { type: String, required: true },
+    timestamp: { type: String, required: false },
+    receiver: Boolean
+
 });
-const whatsappMessage = mongoose.model('messagecontents',whatsappSchema);
-module.exports =  {whatsappMessage}
+const whatsappMessage = mongoose.model('messagecontents', whatsappSchema);
+module.exports = { whatsappMessage }
